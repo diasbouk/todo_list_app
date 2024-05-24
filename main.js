b8d5c3413eac1313407659d0ac41a83285b08cec
diasbouk <boukdsaid@gmail.com>
2024-05-24 03:25:32 +0100

    Classes solved

--- main.js
+++ main.js
@@ -7,22 +7,58 @@ const { createApp, ref } = Vue;
 
 createApp({
 	setup() {
-		const count = ref(0)
-		class Item {
-			id = 0
-			description = ''
+		/* List containing all our todos */
+
+		/* todoItem to hold info */
+		const todoItem = {}
+
+		// Class representing the list
+		class List {
+			/* Class representing a lists
+			 * Attrs:
+			 * 		data -> list of objs (items | todos)
+			 * 		numOfItems -> integer holding the length
+			 * 		todoItemId -> integer to hold id
+			 * 		todoItemDesc -> integer to hold description
+			* */
+			static data = []
+			static numOfItems = 0
+			static todoItemId = 0
+			static todoItemDesc = ''
+
+		static createNew() {
+
+			List.data.push(new Item(todoItem.id, todoItem.desc))
+			for (let ele of List.data) {
+				console.log(ele.id)
+				console.log(ele.description)
+			}
 		}
+		}
+
+
+		// Creating the item class
+		class Item {
+			/* This is the class for items
+			 * id -> integer
+			 * description -> string
+			* */
+
+			constructor(id, description) {
+				/* Constructor for our class
+				takinf two params (instance attributes) */
+				this.id = id
+				this.description = description
+			}
+		}
+
+
+		console.log(todoItem.id)
+		console.log(todoItem.description)
 		return {
-			count,
 			Item,
+			todoItem,
+			List,
 		}
 	}
 }).mount("#app")
-
-
-
-
-
-
-
-
